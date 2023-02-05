1b3a950421e4e37949b3f75390c91897cdaeef6b
AdolfoRoquero <roquero@dhcp-10-250-232-60.harvard.edu>
2023-02-05 12:44:13 -0500

    client server chat app v1 test

--- design_exercise_1/client.py
+++ design_exercise_1/client.py
@@ -1,16 +1,97 @@
 import socket
+import sys
+import errno
+# TODO read IP from config file
 HOST = "10.250.227.245"
 PORT = 6000
-def client_soc ():
-    # socket.gethostname() this returns the local network IP
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect((HOST, PORT))
+
+
+# Size of the header that encodes the username size
+HDR_USERNAME_SZ = 2
+# 
+HDR_DESTINATARIES_SZ = 10 
+HDR_MESSAGE_SZ = 3
+HDR_REQUEST_TYPE_SZ = 1
+HDR_SZ = HDR_USERNAME_SZ + HDR_MESSAGE_SZ + HDR_REQUEST_TYPE_SZ
+# Reply Protocol Definition
+HDR_USERNAME_FROM_SZ = 1
+
+def client_setup():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    return client_socket
+
+def receive_message(scket):
+  try:
+    username_hdr = scket.recv(HDR_USERNAME_SZ)
+    
+    # Connection Error
+    if not (len(username_hdr)):
+        return False
+
+    username_length = int(username_hdr.decode('utf-8').strip())
+    username = scket.recv(username_length).decode('utf-8').strip()
+
+    message_hdr = scket.recv(HDR_MESSAGE_SZ)
+    
+    # Connection Error
+    if not (len(message_hdr)):
+        return False
+
+    message_length = int(message_hdr.decode('utf-8').strip())
+    message = scket.recv(message_length).decode('utf-8').strip()
+    
+    return {
+        'username': username, # decoded
+        'message': message   # decoded
+    }
+           
+  
+  except:
+     return False
+
+
+if __name__ == '__main__':
+    client_soc = client_setup()
+    username = 'Adolfo'
+    busername = username.encode("utf-8")
+    username_hdr = f"{len(busername):<{HDR_USERNAME_SZ}}".encode('utf-8')
 
     msg = "CS 143!!!!"
-    bmsg = msg.encode("ascii")
-    sent = clientsocket.send(bmsg)
-    print('Message sent, %d/%d bytes transmitted' % (sent, len(msg)))
-    print(socket.gethostname())
-    clientsocket.close()
+    bmsg = msg.encode("utf-8")
+    message_hdr = f"{len(bmsg):<{HDR_MESSAGE_SZ}}".encode('utf-8')
 
-client_soc()
+    dest = "Jacobo"
+    bdest = dest.encode("utf-8")
+    dest_hdr = f"{len(bdest):<{HDR_DESTINATARIES_SZ}}".encode('utf-8')
+
+    msg_type = 'R' # 'r'
+    bmsg_type = msg_type.encode("utf-8")
+
+    sent = client_soc.send(username_hdr + busername + 
+                           dest_hdr + bdest +
+                           message_hdr + bmsg + 
+                           bmsg_type)
+    
+    print('Message sent, %d/%d bytes transmitted' % (sent, len(msg)))
+
+
+    try:
+        while True:
+            content = receive_message(client_soc)
+            print(f'Message received from user: {content["username"]}')
+            print(content["message"])
+
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print(f'Reading Error {e}')
+            sys.exit()
+    except Exception as e:
+        print(f'Reading error: {e}')
+        sys.exit()
+
+            
+
+    client_soc.close()
+
--- design_exercise_1/server.py
+++ design_exercise_1/server.py
@@ -6,8 +6,7 @@ print(HOST)
 PORT = 6000
 TIMEOUT = 60 # In seconds
 
-# Protocol Definition
-
+# Request Protocol Definition
 # HEADER:
 # USERNAME_LENGTH -->  16 byte
 # MESSAGE_LENGTH -->  150 byte
@@ -15,15 +14,27 @@ TIMEOUT = 60 # In seconds
 # DATA:
 # USERNAME --> 
 # MESSAGE -->
-HDR_USERNAME_SZ = 16
-HDR_MESSAGE_SZ = 150
+
+# Size of the header that encodes the username size
+HDR_USERNAME_SZ = 2
+# 
+HDR_DESTINATARIES_SZ = 10 
+HDR_MESSAGE_SZ = 3
 HDR_REQUEST_TYPE_SZ = 1
 HDR_SZ = HDR_USERNAME_SZ + HDR_MESSAGE_SZ + HDR_REQUEST_TYPE_SZ
 
-# Stores existing usernames
-usernames = []
 
-# Key: socket, val: username
+# Reply Protocol Definition
+HDR_USERNAME_FROM_SZ = 1
+
+
+# Stores existing usernames 
+# Usernames have a length from 1 to 99 chars
+usernames = ['adolfo', 
+             'le', 
+             'jacobo']
+
+# Key: socket, val: {username: , addr:}
 clients = {}
 
 def server_setup():
@@ -35,35 +46,57 @@ def server_setup():
 
 
 
+def receive_message(scket):
+  try:
+    username_hdr = scket.recv(HDR_USERNAME_SZ)
+    
+    # Connection Error
+    if not (len(username_hdr)):
+        return False
+
+    username_length = int(username_hdr.decode('utf-8').strip())
+    username = scket.recv(username_length).decode('utf-8').strip()
 
 
-def receive_message(socket):
-  socket.recv()
+    destinataries_hdr = scket.recv(HDR_DESTINATARIES_SZ)
+    # Connection Error
+    if not (len(destinataries_hdr)):
+        return False
+
+    destinataries_length = int(destinataries_hdr.decode('utf-8').strip())
+    destinataries = scket.recv(destinataries_length).decode('utf-8').strip().split(',')
+
+
+    message_hdr = scket.recv(HDR_MESSAGE_SZ)
+    
+    # Connection Error
+    if not (len(message_hdr)):
+        return False
+
+    message_length = int(message_hdr.decode('utf-8').strip())
+    message = scket.recv(message_length)
+
+    # TODO Deal with request type
+    request_type = scket.recv(HDR_REQUEST_TYPE_SZ).decode('utf-8').strip()
+    
+    if not (len(request_type)):
+        return False
+    
+    return {'username_hdr': username_hdr,
+            'username': username, # decoded
+            'message_hdr': message_hdr,
+            'message': message,   # encoded
+            'request_type': request_type,  # decoded
+            'destinataries': destinataries} # decoded and converted to list
   
-  
-
- 
-
-
-
-
-def server_():
- client_socket, addr = server_socket.accept()
- print('Connected to by:', addr)
-
- bdata = client_socket.recv(128)
- data = bdata.decode('ascii')
- print(data)
-
+  except:
+     return False
 
 if __name__ == '__main__':
  server_socket = server_setup()
  socket_list = [server_socket]
 
-
-
  while True:
-
     ready_to_read, ready_to_write, in_error = select.select(
                     socket_list,
                     socket_list,
@@ -77,13 +110,38 @@ if __name__ == '__main__':
             # Create new socket for send/receive from client
             conn, addr = server_socket.accept()
 
+            message_content = receive_message(sockt)
+
             # Add new socket to the list of sockets passed to select
             socket_list.append(conn)
 
+            clients[conn] = {'username' : message_content['username'],
+                             'addr': addr}
+
+            print(f"Accepted new connection from user: {message_content['username']} at {addr[0]}:{addr[1]}")
+
+        else:
+            message_content = receive_message(sockt)
+            if not message_content:
+                print(f"Closed connection from user: {clients[sockt]['username']} at {clients[sockt]['addr'][0]}:{clients[sockt]['addr'][1]}")
+                socket_list.remove(sockt)
+                del clients[sockt] 
+                continue
+
+            for sockt, info in clients.items():
+               if info['username'] in message_content['destinataries']:
+                  sockt.send(
+                     message_content['username_hdr'] + 
+                     message_content['username'].encode('utf-8') + 
+                     message_content['message_hdr'] + 
+                     message_content['message'])
+                  
+               
+               
+               
 
 
 
 
-    receive_message(sockt)
-   
+
 
