856e5bb7570c9078516c97574c9976fb6975bf67
leticia <leschettino@gmail.com>
2023-03-08 00:57:01 -0500

    plotter and run experiment

--- design_exercise_2/scale_model.py
+++ design_exercise_2/scale_model.py
@@ -15,7 +15,8 @@ def delete_log_files(dir=os.getcwd()):
         if path.endswith('.log'):
             os.remove(path)	
 
-    
+
+
 
 def setup_logger(name, log_file, level=logging.INFO):
     """To setup as many loggers as you want"""
@@ -30,15 +31,17 @@ def setup_logger(name, log_file, level=logging.INFO):
 
 class Config: 
     def __init__(self, name, host, listening_port, 
-                 sending_ports = [], clock_rate = 1):
+                 sending_ports = [], clock_rate = 1, rand_upper_bound = 9):
        self.name = name
        self.host = host
        self.port = listening_port
        self.out_ports = sending_ports
        self.clock_rate = clock_rate
+       self.rand_upper_bound = rand_upper_bound
 
     def add_pid(self, pid): 
         self.pid = pid 
+    
 
 def consumer(client_socket):
     print("consumer accepted connection" + str(client_socket)+"\n")
@@ -93,7 +96,7 @@ def init_machine(config):
     except Exception as e:
         print("Error accepting client sockets: %s" % e)
 
-def machine(config):
+def machine(config, log_folder):
     config.add_pid(os.getpid())
     global code
     code = []
@@ -105,7 +108,7 @@ def machine(config):
     clock_rate = config.clock_rate
 
     global logger
-    logger = setup_logger(f'{config.name}_pid_{config.pid}', f'{config.name}_pid_{config.pid}.log')
+    logger = setup_logger(f'{config.name}_pid_{config.pid}', f'./{log_folder}/{config.name}_pid_{config.pid}.log')
     logger.info(f"Hello from process {config.name} with pid {config.pid} at rate {clock_rate}")
 
     global msg_queue
@@ -154,9 +157,10 @@ def machine(config):
             msg = msg_queue.pop(0)
             logger.info(f"event: RECEIVE MESSAGE, sys_clock: {clock}, logical_clock: {logical_clock}, code: {-1}, queue_length: {len(msg_queue)}, msg: {msg}")
             logical_clock = max(logical_clock, int(msg))
+            code = [] 
         else: 
             assert len(msg_queue) == 0 
-            action = random.randint(0,9)
+            action = random.randint(0,config.rand_upper_bound)
             if action in list(range(len(config.out_ports))):
                 """ Send to ONE of the other machines a message that is:
                     - the local logical clock time, 
@@ -178,44 +182,45 @@ def machine(config):
    
 
 localHost= "127.0.0.1"
- 
-if __name__ == '__main__':
-    delete_log_files(os.getcwd())
+
+def run_experiment(random_ = True, clock_rates = [1, 1, 1], rand_upper = 9): 
     port1 = 2056
     port2 = 3056
     port3 = 4056
  
+    if random_: 
+        clock_rates = [1 / random.randint(1, 6), 1 / random.randint(1, 6),1 / random.randint(1, 6)]
     # random.seed(262)
+    log_folder =f"/experiment_rates_{'-'.join(['%.2f' % elem for elem in clock_rates])}_rand_{rand_upper}"
+    os.makedirs(f"{os.getcwd()}{log_folder}")
+    print('P1 clock rate: ', clock_rates[0])
+    config1 = Config('P1', localHost, port1, [port2, port3], clock_rates[0], rand_upper)
+    p1 = Process(target=machine, args=(config1, log_folder,))
 
-    clock_rate = 1 / random.randint(1, 6)
-    clock_rate = 1
-    print('P1 clock rate: ', clock_rate)
-    config1 = Config('P1', localHost, port1, [port2, port3], clock_rate)
-    p1 = Process(target=machine, args=(config1,))
-
-    clock_rate = 1 / random.randint(1, 6)
-    clock_rate = 1/3
-
-    print('P2 clock rate: ', clock_rate)
-    config2 = Config('P2', localHost, port2, [port1, port3], clock_rate)
-    p2 = Process(target=machine, args=(config2,))
+    print('P2 clock rate: ', clock_rates[1])
+    config2 = Config('P2', localHost, port2, [port1, port3], clock_rates[1], rand_upper)
+    p2 = Process(target=machine, args=(config2,log_folder, ))
     
-    clock_rate = 1 / random.randint(1, 6)
-    clock_rate = 1/2
-    print('P3 clock rate: ', clock_rate)
-    config3 = Config('P3', localHost, port3, [port1, port2], clock_rate)
-    p3 = Process(target=machine, args=(config3,))
+    print('P3 clock rate: ', clock_rates[2])
+    config3 = Config('P3', localHost, port3, [port1, port2], clock_rates[2], rand_upper)
+    p3 = Process(target=machine, args=(config3,log_folder, ))
     
     p1.start()
     p2.start()
     p3.start()
     
-    # p1.join()
-    # p2.join()
-    # p3.join()
-
-    time.sleep(100)
+    time.sleep(200)
 
     p1.kill()
     p2.kill()
     p3.kill()
+
+
+if __name__ == '__main__':
+    delete_log_files(os.getcwd())
+    rates = [[1,1,1], [1/6, 1/2, 1/2]]
+    for rate_exp in rates: 
+        for rand_upper in [3, 5, 7]: 
+            run_experiment(random_=False, clock_rates = rate_exp, rand_upper=rand_upper)
+
+    
