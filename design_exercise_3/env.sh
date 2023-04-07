adadcd7a28548889f745b5968e479aa30017a355
AdolfoRoquero <roquero@dhcp-10-250-94-1.harvard.edu>
2023-04-07 16:19:14 -0400

    push

--- design_exercise_3/env.sh
+++ design_exercise_3/env.sh
@@ -12,3 +12,14 @@
 #-------------------------------------------------------------------------------
 export CHAT_APP_SERVER_HOST='10.250.94.1'
 export CHAT_APP_SERVER_PORT='50051'
+
+export REP_SERVER_HOST_1='10.250.94.1'
+export REP_SERVER_PORT_1='50001'
+
+export REP_SERVER_HOST_2='10.250.94.1'
+export REP_SERVER_PORT_2='50002'
+
+export REP_SERVER_HOST_3='10.250.94.1'
+export REP_SERVER_PORT_3='50003'
+
+
