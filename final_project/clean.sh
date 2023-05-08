156576b5dcde9e234f862d3f240ce6c071801e63
AdolfoRoquero <roquero@dhcp-10-250-94-1.harvard.edu>
2023-05-07 20:40:14 -0400

    add clean.sh

---
+++ final_project/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Quip2P2lash - CS262 Final Project - RUN 3 NODES
+# This script runs 3 P2P nodes to play the game 
+# (you must have run chmod +x clean.sh BEFORE)
+# 
+
+# remove all .log files
+rm *.log
